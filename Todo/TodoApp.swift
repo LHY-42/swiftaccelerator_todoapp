51ac0efa1798fe2ed763af319bb53f721fabf3a0
hao yuan <95842688+LHY-42@users.noreply.github.com>
2022-06-25 10:37:16 +0800

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by hy loh on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
